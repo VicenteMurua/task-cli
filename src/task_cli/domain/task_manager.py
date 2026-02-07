8b335a710a8b2f83fcbf92a219b10652cf13bc58
VicenteMurua <muruamedina@gmail.com>
2026-02-07 18:44:15 -0300

    feat(task_manager): method implementations

--- src/task_cli/domain/task_manager.py
+++ src/task_cli/domain/task_manager.py
@@ -1,23 +1,34 @@
-from typing import List
-
-from task_cli.domain.task import Task
+from task_cli.domain.task import Task, TaskStatus
 from task_cli.repository.task_repository import ITaskRepository
 
 
 class TaskManager:
     def __init__(self, repository: ITaskRepository) -> None:
         self._repository: ITaskRepository = repository
-        self._lista_tareas: List[Task] = self._repository.load()
+        self._dict_tareas: dict[int,Task] = self._repository.load()
 
     def add(self, descripcion: str) -> None:
-        pass
+        nueva_tarea: Task = Task(descripcion, TaskStatus.TODO)
+        self._dict_tareas[nueva_tarea.identificador] = nueva_tarea
 
     def update(self, identificador: int, descripcion_nueva: str) -> None:
-        pass
+        tarea_objetivo: Task = self._dict_tareas[identificador]
+        tarea_objetivo.cambiar_descripcion(descripcion_nueva)
+        self._dict_tareas.update({identificador: tarea_objetivo})
 
     def delete(self, identificador: int) -> None:
-        pass
+        del self._dict_tareas[identificador]
 
     def mark(self, estado: str, identificador: int) -> None:
-        pass
+        tarea_objetivo: Task = self._dict_tareas[identificador]
+        tarea_objetivo.cambiar_estado(TaskStatus[estado])
+        self._dict_tareas.update({identificador: tarea_objetivo})
 
+    def list(self, filtro: str) -> dict[int, Task]:
+        tareas_filtradas: dict[int, Task] = {}
+        if filtro == "":
+            return self._dict_tareas
+        for identificador, tarea in self._dict_tareas.items():
+            if tarea.status is TaskStatus[filtro]:
+                tareas_filtradas.update({identificador: tarea})
+        return tareas_filtradas
