6df518fa3402d019ccbb742083dc3dd6d8ffeed8
VicenteMurua <muruamedina@gmail.com>
2026-02-07 18:44:59 -0300

    feat(ui_cli): method implementation

--- src/task_cli/ui/ui_cli.py
+++ src/task_cli/ui/ui_cli.py
@@ -13,15 +13,17 @@ class TaskCli:
             "delete": self._cmd_delete,
             "mark-in-progress": self._cmd_mark,
             "mark-done": self._cmd_mark,
+            "mark-todo": self._cmd_mark,
             "list": self._cmd_list,
-
         }
-
-        if len(command) == 1:
+        largo_comando_entrada = len(command)
+        if largo_comando_entrada == 1:
             raise ValueError("Sin argumentos")
 
         comando: str = command[1]
-        detalle: list[str] = command[2:]
+        # TODO: quitar ese fix y habilitar todo el programa para que los typehints acepten null
+        detalle: list[str] = command[2:] if largo_comando_entrada > 2 else [""]
+
         if comando not in comandos:
             raise ValueError("No se ingreso comando válido")
 
@@ -34,7 +36,7 @@ class TaskCli:
         comandos[funcion](argumento)
 
     def _cmd_add(self, descripcion: list[str]) -> None:
-        if len(descripcion) == 0:
+        if descripcion[0] == "":
             raise ValueError("Sin argumentos, se esperaba un argumento para la descripción de la tarea")
         if len(descripcion) > 1:
             raise ValueError("Demasiados argumentos, se esperaba un solo argumento para la descripción, pruebe el uso de comillas")
@@ -60,6 +62,10 @@ class TaskCli:
             raise ValueError(
                 f"Se proporcionaron {elementos} argumentos, se esperaba 1 solo argumentos para el subcomando delete"
                 f" el id de la tarea a eliminar")
+        if descripcion[0] == "":
+            raise ValueError(
+                f"Se proporcionaron 0 argumentos, se esperaba 1 argumento para el subcomando delete"
+                f" el id de la tarea a eliminar")
         identificador: str = descripcion[0]
         try:
             _id: int = int(identificador)
@@ -67,15 +73,19 @@ class TaskCli:
             raise ValueError("El valor asignado al identificador no es un int")
         self._manager.delete(_id)
 
-
     def _cmd_mark(self, descripcion: list[str]) -> None:
-        accion: str = descripcion[0]
-        descripcion: list[str] = descripcion[1:]
         elementos = len(descripcion)
+        accion: str = descripcion[0]
         if elementos != 1:
             raise ValueError(
-                f"Se proporcionaron {elementos} argumentos, se esperaba 1 solo argumentos para el subcomando {accion}"
+                f"Se proporcionaron {elementos} argumentos, se esperaba 1 solo argumentos para el subcomando mark-{accion}"
                 f" el id de la tarea a marcar")
+        if descripcion[0] == "":
+            raise ValueError(
+                f"Se proporcionaron 0 argumentos, se esperaba 1 solo argumentos para el subcomando mark-{accion}"
+                f" el id de la tarea a marcar")
+
+        descripcion: list[str] = descripcion[1:]
         identificador: str = descripcion[0]
         try:
             _id: int = int(identificador)
@@ -84,5 +94,10 @@ class TaskCli:
         self._manager.mark(accion, _id)
 
     def _cmd_list(self, descripcion: list[str]):
-        pass
-
+        cantidad_argumentos = len(descripcion)
+        if cantidad_argumentos != 1:
+            raise ValueError(
+                f"Se esperaba 0 o 1 argumentos para el subcomando list"
+                f" ninguno para listar todos, o para filtrar uno de los siguientes: done, todo, in-progress")
+        for identificador, tarea in self._manager.list(descripcion[0]).items():
+            print(f"{identificador}: {tarea}")
