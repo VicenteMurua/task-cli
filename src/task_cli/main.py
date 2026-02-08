c21c5b32ff8acce282549699d8902591bf54bf91
VicenteMurua <muruamedina@gmail.com>
2026-02-08 18:44:26 -0300

    style(main): APP_NAME -> app_name

--- src/task_cli/main.py
+++ src/task_cli/main.py
@@ -2,14 +2,13 @@ import sys
 from platformdirs import  user_data_dir
 from pathlib import Path
 
-from task_cli.domain.task import Task
 from task_cli.domain.task_manager import TaskManager
 from task_cli.repository.task_repository import JSONTaskRepository, ITaskRepository
 from task_cli.ui.ui_cli import TaskCli
 def main():
 
-    APP_NAME = "task_cli"
-    data_dir = user_data_dir(APP_NAME)
+    app_name = "task_cli"
+    data_dir = user_data_dir(app_name)
     path_dir = Path(data_dir)
     path_dir.mkdir(parents=True, exist_ok=True)
     json_path = path_dir / "task.json"
