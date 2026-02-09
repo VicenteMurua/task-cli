bd29f9a9fb79382f27c3cb9c2e84d8c294c4eb32
VicenteMurua <muruamedina@gmail.com>
2026-02-09 11:23:02 -0300

    style(TaskCli), refactor(_cmd_list), fix(parser_mark,parser_list,manager.filtrar)
    Se arreglo el style del cli ya quedaba muy largo, se refactorizo el comando de lista, se fixeo un problema de que lista no recibia none en el manager, se arreglo un pasaje de un valor que debia ir a setdefault y se pasaba a add argument con status

--- src/task_cli/ui/ui_cli.py
+++ src/task_cli/ui/ui_cli.py
@@ -11,34 +11,51 @@ class TaskCli:
     def __init__(self, manager: TaskManager):
         self._manager = manager
         self._parser = argparse.ArgumentParser(prog="task-cli")
-        subparsers = self._parser.add_subparsers(dest="command")
+        subparsers = self._parser.add_subparsers(dest="command", required=True)
 
         # ------------------------ add ------------------------ #
-        parser_add = subparsers.add_parser("add", help="Agrega una nueva tarea")
-        parser_add.add_argument("descripcion", help="Descripción de la tarea")
+        parser_add = subparsers.add_parser("add",
+            help="Agrega una nueva tarea")
+        parser_add.add_argument("descripcion",
+            help="Descripción de la tarea")
         parser_add.set_defaults(func =self._cmd_add)
 
         # ------------------------ update ------------------------ #
-        parser_update = subparsers.add_parser("update", help="Cambia descripción de tarea")
-        parser_update.add_argument("id", type=int, help="id de la tarea")
-        parser_update.add_argument("descripcion", help="Descripción de la tarea")
+        parser_update = subparsers.add_parser("update",
+            help="Cambia descripción de tarea")
+        parser_update.add_argument("id", type=int,
+            help="id de la tarea")
+        parser_update.add_argument("descripcion",
+            help="Descripción de la tarea")
         parser_update.set_defaults(func =self._cmd_update)
 
         # ------------------------ delete ------------------------ #
-        parser_delete = subparsers.add_parser("delete", help="Elimina una tarea")
-        parser_delete.add_argument("id", type=int, help="id de la tarea")
+        parser_delete = subparsers.add_parser("delete",
+            help="Elimina una tarea")
+        parser_delete.add_argument("id", type=int,
+            help="id de la tarea")
         parser_delete.set_defaults(func =self._cmd_delete)
 
         # ------------------------ mark-done ------------------------ #
-        parser_mark_done = subparsers.add_parser("mark-done", status="done", help="Marca 'done' una tarea")
-        parser_mark_done.add_argument("id", type=int, help="id de la tarea")
-        parser_mark_done.set_defaults(func =self._cmd_mark)
+        parser_mark_done = subparsers.add_parser("mark-done",
+            help="Marca 'done' una tarea")
+        parser_mark_done.add_argument("id", type=int,
+            help="id de la tarea")
+        parser_mark_done.set_defaults(func =self._cmd_mark, status="done")
+
         # ------------------------ mark-in-progress ------------------------ #
-        parser_mark_in_progress = subparsers.add_parser("mark-in-progress", status="in-progress", help="Marca 'in-progress' una tarea")
-        parser_mark_in_progress.add_argument("id", type=int, help="id de la tarea")
-        parser_mark_in_progress.set_defaults(func=self._cmd_mark)
+        parser_mark_in_progress = subparsers.add_parser("mark-in-progress",
+            help="Marca 'in-progress' una tarea")
+        parser_mark_in_progress.add_argument("id", type=int,
+            help="id de la tarea")
+        parser_mark_in_progress.set_defaults(func=self._cmd_mark, status="in-progress")
+
         # ------------------------ list ------------------------ #
-        parser_list = subparsers.add_parser("list", help="Lista todas las tareas")
+        parser_list = subparsers.add_parser("list",
+            help="Lista todas las tareas")
+        parser_list.add_argument("filtro",
+            nargs="?", default=None, choices=["done","in-progress","todo"],
+            help="Filtro de tareas por estado")
         parser_list.set_defaults(func =self._cmd_list)
 
     def run(self) -> None:
@@ -57,13 +74,8 @@ class TaskCli:
     def _cmd_mark(self, args: argparse.Namespace) -> None:
         self._manager.mark(args.status, args.id)
 
-    def _cmd_list(self, descripcion: list[str]):
-        cantidad_argumentos = len(descripcion)
-        if cantidad_argumentos != 1:
-            raise ValueError(
-                f"Se esperaba 0 o 1 argumentos para el subcomando list"
-                f" ninguno para listar todos, o para filtrar uno de los siguientes: done, todo, in-progress")
-        lista_de_tareas: list[Task] = list(self._manager.filtrar(descripcion[0]).values())
+    def _cmd_list(self, args: argparse.Namespace) -> None:
+        lista_de_tareas: list[Task] = list(self._manager.filtrar(args.filtro).values())
         if not lista_de_tareas:
             print("No hay tareas agendadas, en este filtro. Agregue una nueva con 'add'.")
             return
