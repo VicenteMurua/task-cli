84ba2273eeca7ae08bc3ef46963ce68a33dd7730
VicenteMurua <muruamedina@gmail.com>
2026-02-09 09:40:43 -0300

    refactor: run -> init
     se migro la fucnionalidad del diccionario al init para inicializar los estados de los comandos mientras que run solo debera dirigir al parser inerno

--- src/task_cli/ui/ui_cli.py
+++ src/task_cli/ui/ui_cli.py
@@ -9,23 +9,21 @@ class TaskCli:
 
     def __init__(self, manager: TaskManager):
         self._manager = manager
+        self._parser = argparse.ArgumentParser(prog="task-cli")
+        subparsers = self._parser.add_subparsers(dest="command")
+        parser_add = subparsers.add_parser("add")
+        parser_add.set_defaults(func =self._cmd_add)
+        parser_update = subparsers.add_parser("update")
+        parser_update.set_defaults(func =self._cmd_update)
+        parser_delete = subparsers.add_parser("delete")
+        parser_delete.set_defaults(func =self._cmd_delete)
+        parser_mark = subparsers.add_parser("mark")
+        parser_mark.set_defaults(func =self._cmd_mark)
+        parser_list = subparsers.add_parser("list")
+        parser_list.set_defaults(func =self._cmd_list)
 
     def run(self, command: list[str]) -> None:
-        parser = argparse.ArgumentParser(prog="task-cli")
-        subparsers = parser.add_subparsers(dest="command")
-        parser_add = subparsers.add_parser("add")
-        parser_update = subparsers.add_parser("update")
-        parser_delete = subparsers.add_parser("delete")
-        parser_mark = subparsers.add_parser("mark")
-        parser_list = subparsers.add_parser("list")
 
-        comandos:dict[str, Callable[[list[str]], None]] = {
-            "add": self._cmd_add,
-            "update": self._cmd_update,
-            "delete": self._cmd_delete,
-            "mark": self._cmd_mark,
-            "list": self._cmd_list,
-        }
         largo_comando_entrada = len(command)
         if largo_comando_entrada == 1:
             raise ValueError("Sin argumentos")
