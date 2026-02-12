76e416e906a8aefa894dc87e040dcee8d6eb2d5b
VicenteMurua <muruamedina@gmail.com>
2026-02-11 22:24:07 -0300

    refactor: ussing mapper

--- src/task_cli/repository/task_repository.py
+++ src/task_cli/repository/task_repository.py
@@ -1,8 +1,7 @@
 from abc import ABC, abstractmethod
 from pathlib import Path
-from task_cli.domain.task import  TaskStatus, TaskDTO
-from datetime import datetime
-from dataclasses import asdict
+from task_cli.domain.task import  TaskDTO, TaskMapper
+
 import json
 
 class ITaskRepository(ABC):
@@ -20,34 +19,21 @@ class JSONTaskRepository(ITaskRepository):
         self.path: Path = path
 
     def load(self) -> list[TaskDTO]:
-
         if not self.path.exists():
             self.path.write_text("[]", encoding="utf-8")
+
         task_dto_list: list[TaskDTO] = []
         with open(self.path, 'r', encoding='utf-8') as file:
             raw_data = json.load(file)
             for task_entry in raw_data:
-                description: str = task_entry['description']
-                status: TaskStatus = TaskStatus(task_entry['status'])
-                task_id: int = int(task_entry['id'])
-                created_at: datetime = datetime.fromisoformat(task_entry['created_at'])
-                updated_at: datetime = datetime.fromisoformat(task_entry['updated_at'])
-                task_dto = TaskDTO(
-                    description=description,
-                    status=status,
-                    task_id=task_id,
-                    created_at=created_at,
-                    updated_at=updated_at
-                )
+                task_dto: TaskDTO = TaskMapper.from_dict(task_entry)
                 task_dto_list.append(task_dto)
         return  task_dto_list
 
-    def save(self, task_list: list[TaskDTO]) -> None:
-        serialized_task:list[TaskDTO] = []
-        for task in task_list:
-            serialized_task.append(asdict(task))
+    def save(self, task_dto_list: list[TaskDTO]) -> None:
+        serialized_task: list[dict] = []
+        for task_dto in task_dto_list:
+            serialized_task.append(TaskMapper.to_dict(task_dto))
 
         with open(self.path, 'w', encoding='utf-8') as file:
             json.dump(serialized_task, file, ensure_ascii=False, indent=4)
-    def _from_dto_to_json(self, task_dto: TaskDTO) -> dict:
-        pass
