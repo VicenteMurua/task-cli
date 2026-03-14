cd317d204deb2793b0f233c9d97a14cfc999d7fd
VicenteMurua <muruamedina@gmail.com>
2026-03-14 01:23:06 -0300

    chore: import

--- src/task_cli/main.py
+++ src/task_cli/main.py
@@ -1,16 +1,18 @@
 from task_cli.domain.task_manager import TaskManager
 from task_cli.infrastructure.factories import make_task_repository, make_config_manager
-from task_cli.infrastructure.enums import RepoType
 from task_cli.repository.task_repository import IRepository
 from task_cli.ui.command_interface import CommandInterface
 
 
-def main():
+def on_cli():
     config_manager = make_config_manager()
     repo: IRepository = make_task_repository(config_manager)
     task_manager = TaskManager(repo)
     cli_handler = CommandInterface(task_manager, config_manager)
     cli_handler.run()
+
+def main():
+    on_cli()
 
 if __name__ == "__main__":
     main()
